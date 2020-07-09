c1df5b569ac301724eb5d3e9a30cbc78ad5e86e0
wasant <wasant.wsmart@gmail.com>
2020-07-09 09:42:30 +0700

    initial commit

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,24 +9,24 @@ use App\Event;
 class EventController extends Controller
 {
     //
-    $events = [];
-    $data = Event::all();
-    if($data->count()) {
-        foreach ($data as $key => $value) {
-            $events[] = Calendar::event(
-                $value->title,
-                true,
-                new \DateTime($value->start_date),
-                new \DateTime($value->end_date.' +1 day'),
-                null,
-                // Add color and link on event
-                [
-                    'color' => '#f05050',
-                    'url' => 'pass here url and any route',
-                ]
-            );
-        }
-    }
-    $calendar = Calendar::addEvents($events);
-    return view('fullcalender', compact('calendar'));
+    // $events = [];
+    // $data = Event::all();
+    // if($data->count()) {
+    //     foreach ($data as $key => $value) {
+    //         $events[] = Calendar::event(
+    //             $value->title,
+    //             true,
+    //             new \DateTime($value->start_date),
+    //             new \DateTime($value->end_date.' +1 day'),
+    //             null,
+    //             // Add color and link on event
+    //             [
+    //                 'color' => '#f05050',
+    //                 'url' => 'pass here url and any route',
+    //             ]
+    //         );
+    //     }
+    // }
+    // $calendar = Calendar::addEvents($events);
+    // return view('fullcalender', compact('calendar'));
 }
