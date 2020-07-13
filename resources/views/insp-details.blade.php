b2264558e4ee42d5c06ae23a5e1517661f734821
Kantee Charoensedtasin <kantee2540@hotmail.com>
2020-07-13 16:24:03 +0700

    Fix route to carshow

--- resources/views/insp-details.blade.php
+++ resources/views/insp-details.blade.php
@@ -108,7 +108,7 @@
                     <div class="nav nav-tabs" id="nav-tab" role="tablist">
                         <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true"><i class="fa fa-picture-o" aria-hidden="true"></i> Picture</a>
                         <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false"><i class="fa fa-camera" aria-hidden="true"></i> Pic 360</a>
-                        <a href="{{ route('car_show') }}" class="btn btn-sm btn-report">Inspection Report</a>
+                        <a href="{{ route('car_show.index') }}" class="btn btn-sm btn-report">Inspection Report</a>
                     </div>
                 </nav>
                 <div class="tab-content" id="nav-tabContent">
